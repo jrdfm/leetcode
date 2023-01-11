544ac523c235023562c5daa5bacb2f1c1c12e5c9
jrdfm <yaredfm@gmail.com>
2023-01-11 18:43:43 -0500

    uniqchar

--- strings.py
+++ strings.py
@@ -16,12 +16,35 @@ def reverse(x):
     d = sum(d * 10 ** i for i,d in enumerate([int(i) if x > 0 else -int(i) for i in str(abs(x)) ]))
     return d if d >= -2**31 and d <= 2 ** 31 - 1 else 0
 
+# Way better
 def reverse(x):
     d = [-1,1][x > 0] * int(str(abs(x))[::-1])
     return d if d >= -2**31 and d <= 2 ** 31 - 1 else 0
 
+'''Given a string s, find the first non-repeating character in it and return its index. 
+   If it does not exist, return -1.'''
+
+# def firstUniqChar(s):
+#     d = {}
+#     for i in s:
+#         d[i] = d.get(i, 0) + 1
+#     for key in d.keys():
+#         if d[key] == 1:
+#             return s.index(key)
+#     return -1        
+
+# Better
+def firstUniqChar(s):
+    idx = [s.index(i) for i in set(s) if s.count(i) == 1]
+    print(set(s))
+    print(idx)
+    return min(idx) if len(idx) > 0 else -1    
 
 if __name__ == '__main__':
-    s = list("TestString")
-    reverseString(s)
-    print(s)
+    # s = list("TestString")
+    # reverseString(s)
+    # print(s)
+
+    # s = "loveleetcode"
+    s = "leetcode"
+    print(firstUniqChar(s))
