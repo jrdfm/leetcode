0223ba6aa21334712ba4df00253a07b3351ec170
jrdfm <yaredfm@gmail.com>
2023-02-03 17:50:28 -0500

    .

--- binarysearch.py
+++ binarysearch.py
@@ -8,7 +8,7 @@ def binarysearch_nxtsm(A,TARGET):
     nxs = TARGET
     while L <= R:
         C = (L+R)//2
-        print(f'C: {C} SubArr: {A[L:R+1]}')
+        # print(f'C: {C} SubArr: {A[L:R+1]}')
         if TARGET < A[C]:
             R = C-1
         elif TARGET > A[C]:
@@ -28,8 +28,7 @@ if __name__ == "__main__":
     # print(A)
     # TARGET = 9
     
-    # A = random.sample(range(0, 100), 50)
-    # A.sort()
+
     # TARGET = A.pop(random.randint(0,len(A) -1))
 
     # print(f'TARGET {TARGET}')
@@ -40,18 +39,15 @@ if __name__ == "__main__":
     # TARGET = A.pop(r := random.randint(0,len(A) -1))
     # print(f'A[r-1] {A[r - 1]}, nxs {binarysearch_nxtsm(A, TARGET)}')
 
-    # for i in range(1000):
-    #     TARGET = A.pop(r := random.randint(0,len(A) -1))
-    #     print(A[r - 1], binarysearch_nxtsm(A, TARGET))
-        # assert(A[r - 1] == binarysearch_nxtsm(A, TARGET))
+    for i in range(1000):
+        A = random.sample(range(0, 100), 50)
+        A.sort()
+        TARGET = A.pop(r := random.randint(0,len(A) -1))
+        # print(A[r - 1], binarysearch_nxtsm(A, TARGET))
+        if r != 0:
+            assert(A[r - 1] == binarysearch_nxtsm(A, TARGET))
 
-    B = [3, 5, 8, 9, 12, 13, 16, 19, 21, 24, 26, 27, 30, 35, 36, 37, 38, 42, 45, 
-        47, 49, 51, 52, 53, 54, 57, 58, 59, 60, 61, 64, 66, 68, 71, 73, 74, 75, 76, 
-        79, 80, 82, 84, 85, 88, 90, 91, 92, 93, 95]
 
-    print(B)
-    TARGET = 4
-    print(binarysearch_nxtsm(B, TARGET))
 
 
 
